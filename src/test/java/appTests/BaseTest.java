ffa2438c233219a5897be50c8570d6e14ef379a3
QA academy <education@qaacademy.rs>
2025-09-25 19:20:11 +0200

    Changed Web browser

--- src/test/java/appTests/BaseTest.java
+++ src/test/java/appTests/BaseTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.BasePage;
 
 import java.time.Duration;
@@ -16,7 +17,7 @@ public class BaseTest {
 
     @BeforeClass
     public static void beforeAll() {
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 //        max time to wait for a page load
